4374875195e2e588f878dbf4884e5d90ad746ed4
marchellodev <marchellodev@gmail.com>
2023-04-30 16:24:47 +0300

    init commit

---
+++ run.sh
@@ -0,0 +1,10 @@
+pyftsubset font.woff --output-file=font2.woff2 --unicodes-file=latin1.txt --flavor=woff2
+
+# custom.txt:
+# https://www.alphabet-type.com/tools/charset-builder/
+# MacOS Std  + Latin-1 + Croatian + Cyrillic + Romanian + Symbol + Turkish + Ukraine
+# + iso 8859 - Central European + Ukrainian Cyrillic ISO
+# total: 609 glyphs
+#
+# also maybe Basic Latin, Latin-1 Supplement and Latin Extended-A
+
